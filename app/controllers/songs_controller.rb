2df3ae12369df069f13f63e6be9c89a761cd2c51
Thomas Hart <myrridin@gmail.com>
2017-10-07 21:27:55 -0400

    Little more error handling

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,35 +1,41 @@
 class SongsController < ApplicationController
   def create
-    song = Song.create(name: params[:title])
+    begin
+      song = Song.create(name: params[:title])
 
-    unless song.id
-      render json: {success: false, error: 'Could not create song'}, status: :bad_request
+      unless song.id
+        render json: {success: false, error: 'Could not create song'}, status: :bad_request
+        return
+      end
+
+      line_index = 1
+      lines = {}
+
+      params[:lyrics].each do |line|
+        line = Line.create!(words: line, index: line_index, song: song)
+
+        unless line.id
+          render json: {success: false, error: "Could not create line #{line_index}"}, status: :bad_request
+          return
+        end
+
+        lines[line_index] = line.id
+        line_index += 1
+      end
+
+      params[:chords].each do |chord_hash|
+        chord_placement = ChordPlacement.create(chord: chord_hash[:chord], line_id: lines[chord_hash[:line]], position: chord_hash[:position])
+
+        unless chord_placement.id
+          render json: {success: false, error: "Could not create chord with params #{chord_hash}"}, status: :bad_request
+          return
+        end
+      end
+    rescue StandardError
+      render json: {success: false, error: "There was an error creating this song."}, status: :bad_request
       return
     end
 
-    line_index = 1
-    lines = {}
-
-    params[:lyrics].each do |line|
-      line = Line.create!(words: line, index: line_index, song: song)
-
-      unless line.id
-        render json: {success: false, error: "Could not create line #{line_index}"}, status: :bad_request
-        return
-      end
-
-      lines[line_index] = line.id
-      line_index += 1
-    end
-
-    params[:chords].each do |chord_hash|
-      chord_placement = ChordPlacement.create(chord: chord_hash[:chord], line_id: lines[chord_hash[:line]], position: chord_hash[:position])
-
-      unless chord_placement.id
-        render json: {success: false, error: "Could not create line with params #{chord_hash}"}, status: :bad_request
-        return
-      end
-    end
 
     render json: {success: true, id: song.id}
   end
